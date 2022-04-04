ec7b51fdd47e38741f5ded9a7a5a4e5a0872094d
Debdeep Banerjee <banerjeedebdeep0@gmail.com>
2022-04-04 18:09:33 +0530

    file-path

--- src/main/java/com/packetprep/projects/Controllers/ViewController.java
+++ src/main/java/com/packetprep/projects/Controllers/ViewController.java
@@ -1,22 +1,33 @@
 package com.packetprep.projects.Controllers;
 
 import com.packetprep.projects.Service.FileService;
+import com.packetprep.projects.ShellCommands;
+import org.json.JSONObject;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 @RestController
 public class ViewController {
 
     @Autowired
     private FileService fileService;
+    @Autowired
+    private ShellCommands shellCommands;
 
 //    @Value("${file.upload.location}")
 //    private String uploadLocation;
@@ -28,37 +39,47 @@ public class ViewController {
     }
 
     @PostMapping("/uploadFile")
-    public String uploadFile(@RequestParam("myFile") MultipartFile myFile) throws IOException {
-//        if (!myfile.isEmpty() && isRepositoryExists()) {
-//            String filename = myfile.getOriginalFilename();
-//            OutputStream outputStream  = new FileOutputStream(uploadLocation+filename);
-//            outputStream.write(myfile.getBytes());
-//            Files files = new Files(filename, myfile.getContentType());
-//            fileRepository.save(files);
-//        }
+    public HashMap<String, HashSet<String>> uploadFile(@RequestParam("myFile") MultipartFile myFile) throws IOException {
+
         try{
-            fileService.save(myFile);
-            fileService.scan();
-            return "File Uploaded";
+          return fileService.save(myFile);
+
+//            String fileName = myFile.getOriginalFilename();
+//            return ("File Uploaded -----" + fileName);
+
         }catch(Exception e){
-            return "File Not Uploaded";
+//            return "File Not Uploaded";
+            HashMap<String,HashSet<String>> errMap = new HashMap<>();
+            HashSet<String> errMessage = new HashSet<>();
+            errMessage.add("File Not Uploaded!");
+            errMap.put("error",errMessage);
+            return errMap;
+//
         }
 
     }
-    @GetMapping("/scan")
-    public void scan() {
-        fileService.scan();
-    }
-//    public boolean isRepositoryExists() {
-//        File file = new File(uploadLocation);
-//        if (!file.exists()) {
-//            if (file.mkdir()) {
-//                System.out.println("Directory is created!");
-//                return true;
-//            } else {
-//                System.out.println("Failed to create directory!");
-//                return false;
-//            }
-//        } else return true;
+//    @GetMapping("/scan")
+//    public HashMap<String, HashSet<String>> scan() {
+//        return fileService.scan();
 //    }
+
+
+
+    @GetMapping("/copy")
+    public void copyValidator() {
+        fileService.putValidator();
+    }
+
+    @GetMapping("/getData")
+    public ResponseEntity<String> getData() {
+
+        String url= "http://localhost:8081/CRUD/Validate.jsp";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.getForEntity(url, String.class);
+//        System.out.println(result);
+        return result;
+//        JSONParser parser = new JSONParser(result);
+//        JSONObject json = (JSONObject) parser.parse(result);
+//        return Arrays.asList(countries);
+    }
 }
--- src/main/java/com/packetprep/projects/Service/FileService.java
+++ src/main/java/com/packetprep/projects/Service/FileService.java
@@ -1,18 +1,19 @@
 package com.packetprep.projects.Service;
 
+import org.apache.commons.io.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.io.*;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.lang.reflect.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 @Service
 public class FileService {
@@ -21,51 +22,98 @@ public class FileService {
     private String uploadPath;
 
 
+    @Value("${file.store.location}")
+    private String storePath;
+
+    @Value("${file.validator.location}")
+    private String validatorPath;
+
+    @Value("${file.deploy.location}")
+    private String deployPath;
+
+    public static HashSet<String> filenames = new HashSet<>();
+    public static HashSet<String> jspFilenames = new HashSet<>();
+    public  HashMap<String,HashSet<String>> map = new HashMap<>();
+
     @PostConstruct
     public void init() {
         try {
-            Files.createDirectories(Paths.get(uploadPath));
+
+            Files.createDirectories(Paths.get(deployPath));
         } catch (IOException e) {
             throw new RuntimeException("Could not create upload folder!");
         }
     }
 
-    public void save(MultipartFile file) {
+    public HashMap<String,HashSet<String>> save(MultipartFile file) {
         try {
-            Path root = Paths.get(uploadPath);
+            String fname = file.getOriginalFilename();
+
+
+            File theDir = new File(deployPath+"/"+fname);
+            if (!theDir.exists()){
+                theDir.mkdirs();
+            }
+
+            int index = fname.indexOf('.');
+            Path root = Paths.get(deployPath+"/"+fname);
             if (!Files.exists(root)) {
                 init();
+
             }
             Files.copy(file.getInputStream(), root.resolve(file.getOriginalFilename()));
-//
 
+            display(file.getOriginalFilename());
+            scan(fname);
+            return map;
 
         } catch (Exception e) {
             throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+
         }
     }
+    public void putValidator(){
+        try{
+            String name="hello";
+            File source = new File(validatorPath);
 
-    public void scan(){
-//        Collection<File> all = new ArrayList<File>();
-//        addTree(this.uploadPath, all);
-//        System.out.println(all);
-        File actual = new File(uploadPath + "/CRUD");
+            for(File f : source.listFiles()){
+                if(f.listFiles() != null){
+                    File dest = new File(deployPath + "\\WEB-INF\\classes\\com");
+                    FileUtils.copyDirectory(source, dest);
+                }else{
+                    File dest = new File(deployPath);
+                    FileUtils.copyFileToDirectory(f, dest);
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void scan(String fname){
+
+//        File actual = new File(deployPath + "\\WEB-INF\\classes\\com");
+        File actual = new File(deployPath+"/"+fname);
         for( File f : actual.listFiles()){
-//            System.out.println( f.getName() );
+
             if(f.listFiles() != null){
                 scanDirectory(f);
             }else{
                 scanClass(f);
             }
         }
+
+        map.put("servlets",filenames);
+        map.put("jsp",jspFilenames);
+//        filenames.clear();
+//        jspFilenames.clear();
+//        return map;
     }
     public void scanDirectory(File f){
-//        Collection<File> all = new ArrayList<File>();
-//        addTree(this.uploadPath, all);
-//        System.out.println(all);
-        File actual = new File(uploadPath + "/CRUD");
+
+//        File actual = new File(uploadPath + "/CRUD");
         for( File fl : f.listFiles()){
-//            System.out.println( fl.getName() );
+
             if(fl.listFiles() != null) {
                 scanDirectory(fl);
             }else{
@@ -90,52 +138,98 @@ public class FileService {
 
                 String last= name.substring(j);
                 String first= name.substring(0,j);
-//                System.out.println(first);
+
                 if(last.equalsIgnoreCase(".java")){
-                    System.out.println(first);
-                    BufferedReader br
-                            = new BufferedReader(new FileReader(f));
+//                    System.out.println(first);
+                    filenames.add(first);
+//                    BufferedReader br
+//                            = new BufferedReader(new FileReader(f));
+//
 //                    String st;
+//                    // Condition holds true till
+//                    // there is character in a string
 //                    while ((st = br.readLine()) != null)
 //
+//                        // Print the string
+//                        System.out.println(st);
+
+                }else if(last.equalsIgnoreCase(".jsp")){
+//                    System.out.println(first);
+                    jspFilenames.add(first);
+//                    BufferedReader br
+//                            = new BufferedReader(new FileReader(f));
+//
+//                    String st;
+//                    // Condition holds true till
+//                    // there is character in a string
+//                    while ((st = br.readLine()) != null)
+//
+//                        // Print the string
 //                        System.out.println(st);
 
                 }
             }
 
-
-//            if(f.listFiles() == null){
-//                String thisClassname = f.getName();
-//                System.out.println(thisClassname);
-//            }
-//            Class thisClass = thisClassname.class;
         } catch (Throwable e) {
             System.err.println(e);
             return;
         }
     }
 
-//    public void addTree(Path directory, Collection<Path> all)
-//            throws IOException {
-//        try (DirectoryStream<Path> ds = Files.newDirectoryStream(directory)) {
-//            for (Path child : ds) {
-//                all.add(child);
-//                if (Files.isDirectory(child)) {
-//                    addTree(child, all);
-//                }
-//            }
-//        }
-//    }
-//    public void testlinux() throws IOException {
-//
-//        try {
-//
-//            ProcessBuilder pb = new ProcessBuilder("/usr/local/bin/wkhtmltopdf"," http://www.yahoo.com /tmp/yahoo.pdf");
-//
-//            pb.start();
-//
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//    }
+    public void display(String fileName) throws Exception {
+
+        String extract = "\"cmd.exe\", \"/c\", \"cd \"C:\\Users\\Packetprep\\Desktop\\Deploy\\"+ fileName+"\\ && jar -xvf "+ fileName;
+        System.out.println(extract);
+        ProcessBuilder builder = new ProcessBuilder(
+                "cmd.exe", "/c", "cd \"C:\\Users\\Packetprep\\Desktop\\Deploy\\"+ fileName+"\" && jar -xvf "+ fileName);
+
+//        ProcessBuilder builder = new ProcessBuilder(
+//                "cmd.exe", "/c", "cd \""+ deployPath + "\""+fileName+" && jar -xvf "+ fileName);
+
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) { break; }
+            System.out.println(line);
+        }
+
+        p.destroyForcibly();
+        System.out.println(p.isAlive());
+
+//        putValidator();
+        long start = System.currentTimeMillis();
+        long end = start + 3*1000;
+        while (System.currentTimeMillis() < end) {
+            // Some expensive operation on the item.
+        }
+        String pack = "\"cmd.exe\", \"/c\", \"cd \\\"C:\\\\Users\\Packetprep\\Desktop\\Deploy\\"+ fileName+"\\ && jar -cvf verified"+ fileName+" *";
+        System.out.println(pack);
+        ProcessBuilder builder1 = new ProcessBuilder(
+                "cmd.exe", "/c", "cd \"C:\\Users\\Packetprep\\Desktop\\Deploy\\"+fileName+"\" && jar -cvf verified"+ fileName +" *");
+
+
+//        ProcessBuilder builder1 = new ProcessBuilder(
+//                "cmd.exe", "/c","cd \""+ deployPath + "\""+fileName+" && jar -cvf verified"+ fileName +" *");
+
+        builder1.redirectErrorStream(true);
+        Process p1 = builder1.start();
+        System.out.println(p1.isAlive());
+
+        BufferedReader r1 = new BufferedReader(new InputStreamReader(p1.getInputStream()));
+        String line1;
+        while (true) {
+            line1 = r1.readLine();
+            if (line1 == null) { break; }
+            System.out.println(line1);
+        }
+        p1.destroyForcibly();
+        System.out.println(p1.isAlive());
+
+    }
+
+
 }
